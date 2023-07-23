f70516e4c98206a9bfe2891547f1472e37b0e047
MarcoMendza <marco.mendza@outlook.com>
2023-07-22 22:27:17 -0600

    Added JournalLayout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,15 @@
 import { MailLockOutlined } from '@mui/icons-material'
 import { Typography } from '@mui/material'
 import React from 'react'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography>Journal App</Typography>    
-    </>
+    <JournalLayout>
+
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat blanditiis praesentium animi veritatis aliquid, soluta similique iure facere, est magni sapiente. Suscipit tenetur neque reiciendis aspernatur quisquam ullam earum aliquid.</Typography>
+    
+    </JournalLayout>
 
   )
 }
