6296581be3a7209ef84234d441a757635b8584e2
MarcoMendza <marco.mendza@outlook.com>
2023-07-29 17:29:17 -0600

    Saved note to Firestore

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -3,19 +3,32 @@ import { IconButton, Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView} from '../view'
+import { useDispatch, useSelector } from 'react-redux'
+import { startNewNote } from '../../store/journal'
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+  const { isSaving, active } = useSelector( state => state.journal )
+
+  const onClickNewNote = () => {
+    dispatch( startNewNote() )
+  }
+
+
   return (
     <JournalLayout>
-
-      {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat blanditiis praesentium animi veritatis aliquid, soluta similique iure facere, est magni sapiente. Suscipit tenetur neque reiciendis aspernatur quisquam ullam earum aliquid.</Typography>
-       */}
-    
-       <NothingSelectedView/>
-       {/* <NoteView/> */}
+          
+       {
+        (!!active)
+         ? <NoteView/> 
+         : <NothingSelectedView/>
+       }
 
        <IconButton 
+        onClick={ onClickNewNote }
         size='large'
+        disabled= { isSaving }
         sx={{
           color: 'white',
           backgroundColor: 'error.main',
