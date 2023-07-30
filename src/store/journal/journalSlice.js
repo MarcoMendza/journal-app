fa5b188fbf801750eb45941b3998fb58ad5ed6ae
MarcoMendza <marco.mendza@outlook.com>
2023-07-29 18:39:06 -0600

    Loading notes

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -19,8 +19,8 @@ export const journalSlice = createSlice({
         setActiveNote:( state, action ) => {
             state.active = action.payload;
         },
-        setNotes:( state, action ) => {
-            
+        setNotes:( state, action ) => {            
+            state.notes = action.payload
         },
         setSaving:( state, action ) => {
             
